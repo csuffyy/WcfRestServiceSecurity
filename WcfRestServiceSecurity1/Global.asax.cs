8fb2a96c59d347eb729ad80544e6b813cd9397e5
csuffyy <csu-lzy@163.com>
2014-07-18 20:06:29 +0800

    初始版
    初始版
    http://blog.csdn.net/fangxing80/article/details/6263780

---
+++ WcfRestServiceSecurity1/Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+using System.ServiceModel;
+using System.Configuration;
+using System.ServiceModel.Web;
+using System.Net;
+
+namespace WcfRestServiceSecurity1
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            //RouteTable.Routes.Add(new ServiceRoute("TaskService",
+            //    new WebServiceHostFactory(), typeof(TaskService)));
+
+            var securewebServiceHostFactory = new SecureWebServiceHostFactory();
+            RouteTable.Routes.Add(new ServiceRoute("TaskService",
+                securewebServiceHostFactory, typeof(TaskService)));
+        }
+    }
+
+    public class SecureWebServiceHostFactory : WebServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            var host = base.CreateServiceHost(serviceType, baseAddresses);
+            host.Authorization.ServiceAuthorizationManager = new MyServiceAuthorizationManager();
+            return host;
+        }
+
+        public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
+        {
+            var host = base.CreateServiceHost(constructorString, baseAddresses);
+            host.Authorization.ServiceAuthorizationManager = new MyServiceAuthorizationManager();
+            return host;
+        }
+    }
+
+    public class MyServiceAuthorizationManager : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            var ctx = WebOperationContext.Current;
+            var auth = ctx.IncomingRequest.Headers[HttpRequestHeader.Authorization];
+            if (string.IsNullOrEmpty(auth) || auth != "fangxing/123")
+            {
+                ctx.OutgoingResponse.StatusCode = HttpStatusCode.MethodNotAllowed;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WcfRestServiceSecurity1/TaskService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Net;
+
+namespace WcfRestServiceSecurity1
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class TaskService
+    {
+        private static bool CheckAuthorization()
+        {
+            var ctx = WebOperationContext.Current;
+            var auth = ctx.IncomingRequest.Headers[HttpRequestHeader.Authorization];
+            if (string.IsNullOrEmpty(auth) || auth != "fangxing/123")
+            {
+                ctx.OutgoingResponse.StatusCode = HttpStatusCode.MethodNotAllowed;
+                return false;
+            }
+            return true;
+        }
+
+        [WebGet(UriTemplate = "All")]
+        public List<Task> GetTask()
+        {
+            //可以省略了
+            //if (!CheckAuthorization())
+            //    return null;
+            return GetData();
+        }
+
+        [WebGet(UriTemplate = "{taskId}")]
+        public Task GetTaskById(string taskId)
+        {
+            //可以省略了
+            //if (!CheckAuthorization())
+            //    return null;
+            return GetData().FirstOrDefault(t => t.Id==taskId);
+        }
+
+        private List<Task> GetData()
+        {
+            return new List<Task>
+            {
+                new Task { Id="1", Name="Task1" },
+                new Task { Id="2", Name="Task2" },
+                new Task { Id="3", Name="Task3" },
+            };
+        }
+    }
+
+    [DataContract]
+    public class Task
+    {
+        [DataMember]
+        public string Id
+        { get; set; }
+        [DataMember]
+        public string Name
+        { get; set; }
+    }
+
+    [DataContract]
+    public class UserInfo
+    {
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
